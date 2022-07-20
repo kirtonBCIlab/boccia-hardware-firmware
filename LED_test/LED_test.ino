bd8f20ff398072125f1b7a94ee1bd6ac4b3b57df
Daniel Comaduran <dcomadur@ucalgary.ca>
2022-07-19 18:24:45 -0600

    Create LED_test.ino
    Simple script to turn built-in and external LEDs

---
+++ LED_test/LED_test.ino
@@ -0,0 +1,19 @@
+void setup() {
+  // Initialize LEDs
+  pinMode(3, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+
+  digitalWrite(3, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(3, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+
+}
