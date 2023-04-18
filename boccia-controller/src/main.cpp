a60a31111557ce701df0cb1fe12a612f418ab29c
Daniel Comaduran <dcomadur@ucalgary.ca>
2023-04-17 20:53:15 -0600

    Made small changes for Ion to debug the hardware

--- boccia-controller/src/main.cpp
+++ boccia-controller/src/main.cpp
@@ -8,33 +8,33 @@
 int n_steps = 200;
 int dir = 1;
 
-// Change number of pins
-// Define incline actuator
+// Build motor objects
+// - Nema 17
+int nema17_pin_step = 5;
+int nema17_pin_dir = 6;
+BocciaStepper nema17(AccelStepper::DRIVER, nema17_pin_step, nema17_pin_dir);
+
+// - Nema 23
+int nema23_pin_step = 12;
+int nema23_pin_dir = 11;
+BocciaStepper nema23(AccelStepper::DRIVER, nema23_pin_step, nema23_pin_dir);
+
+// - Incline actuator
 int incline_pin1 = 7;
-int incline_pin2 = 6;
-int incline_pin_pot = 0;
+int incline_pin2 = 8;           
+int incline_pin_pot = 2;          // Analog pin for potentiometer
 int incline_speed_threshold = 15;
 int incline_speed_factor = 50;
-int incline_pin_sensor = 18;      // If pin sensor is enabled (i.e., !0), the calibration depends on the pin sensor trigger
+int incline_pin_sensor = 0;       // If pin sensor is enabled (i.e., !0), the calibration depends on the pin sensor trigger
 LinearActuator inclineActuator(incline_pin1, incline_pin2, incline_pin_pot, incline_speed_threshold, incline_speed_factor, incline_pin_sensor);
 
-// Define elevator actuator
-int elevator_pin1 = 5;
-int elevator_pin2 = 4;
-int elevator_pin_pot = 1;
+// - Elevator actuator
+int elevator_pin1 = 9;
+int elevator_pin2 = 10;
+int elevator_pin_pot = 3;
 int elevator_speed_threshold = 15;
 int elevator_speed_factor = 50;
 LinearActuator elevatorActuator(elevator_pin1, elevator_pin2, elevator_pin_pot, elevator_speed_threshold, elevator_speed_factor);
-
-// Define nema17
-int pin_step = 52;
-int pin_dir = 53;
-BocciaStepper nema17(AccelStepper::DRIVER, pin_step, pin_dir);
-
-// Define nema23
-int nema23_pin_step = 50;
-int nema23_pin_dir = 51;
-BocciaStepper nema23(AccelStepper::DRIVER, nema23_pin_step, nema23_pin_dir);
 
 // Prototype functions
 void nema17Limit();
@@ -47,48 +47,51 @@ void setup() {
   Serial.begin(9600);
   Serial.println("Begin setup");
 
-  // Nema17 settings
+  // Set motor settings
+  // - Nema 17
   nema17.setReturnSteps(10);
-  nema17.setDefaultSpeed(400);   // Default speed [steps/sec]
+  nema17.setDefaultSpeed(400);      // Default speed [steps/sec]
   nema17.setDefaultAccel(10);
-  nema17.setMaxSpeed(1000);    // Maximum speed [steps/sec]
+  nema17.setMaxSpeed(1000);         // Maximum speed [steps/sec]
   nema17.setInterruptPin(2);
-  nema17.setNoSteps(200);      // Number of steps for complete rotation [steps]
+  nema17.setNoSteps(200);           // Number of steps for complete rotation [steps]
+  digitalWrite(nema17_pin_dir, 0);  // Set pins to ground to avoid that initial jump
+  digitalWrite(nema17_pin_step, 0);
 
-  // Nema23 settings
+  // - Nema 23
   nema23.setReturnSteps(10);
-  nema23.setDefaultSpeed(400);   // Default speed [steps/sec]
+  nema23.setDefaultSpeed(400);  // Default speed [steps/sec]
   nema23.setDefaultAccel(10);
-  nema23.setMaxSpeed(1000);    // Maximum speed [steps/sec]
+  nema23.setMaxSpeed(1000);     // Maximum speed [steps/sec]
   nema23.setInterruptPin(3);
-  nema23.setNoSteps(800);      // Number of steps for complete rotation [steps]
-
-  // Set NEMA inputs to ground
-  digitalWrite(pin_dir, 0);
-  digitalWrite(pin_step, 0);
+  nema23.setNoSteps(800);       // Number of steps for complete rotation [steps]
 
   // Interrupts
   attachInterrupt(digitalPinToInterrupt(nema17.getInterruptPin()), nema17Limit, RISING);
   attachInterrupt(digitalPinToInterrupt(nema23.getInterruptPin()), nema23Limit, RISING);
 
+  // Calibration steps - Enable sections as needed
   Serial.println("Calibration");
-  // Serial.println("nema17 - Calibration started");
+
+  // - Nema 17
+  // Serial.println("Nema 17 - Calibration started");
   // nema17.findRange();
-  // Serial.println("nema17 - Calibration ended");
-
-  // Serial.println("nema23 - Calibration started");
+  // Serial.println("Nema 17 - Calibration ended");
+ 
+  // - Nema 23
+  // Serial.println("Nema 23 - Calibration started");
   // nema23.findRange();
-  // Serial.println("nema23 - Calibration ended");
+  // Serial.println("Nema 23 - Calibration ended");
 
-  Serial.println("Calibrating - nema17");
-  nema17.findRange();
-  Serial.println("nema17 - Calibration ended");
-
-  Serial.println("Calibrating -  incline actuator");
+  // - Incline actuator
+  Serial.println("Incline - Calibration started");
   inclineActuator.findRange();
-  Serial.println("Calibration ended");
+  Serial.println("Incline - Calibration ended");
 
-  // Add elevator motor
+  //  - Elevator actuator
+  Serial.println("Elevator - Calibration started");
+  elevatorActuator.findRange();
+  Serial.println("Elevator - Calibration ended");
 
   Serial.println("\nSelect motor and movement...");
 }
@@ -100,7 +103,7 @@ void loop()
     decodeCommand();
   }
 
-  waitMillis(250);
+  waitMillis(250);  // Wait a bit while decoding command
 }
 
 
@@ -114,11 +117,18 @@ void nema23Limit()
   nema23.limitDetected();
 }
 
+/// @brief The input command must be a positive or negative number with 
+/// 4 digits. The first digit selects the motor according to the switch
+/// case function inside this function. The other three digits select 
+/// the number of positive or negative steps for the stepper motors, or
+/// the percentage of the linear actuator (where 0\% is retracted, and
+/// 100\% is extended). 
+/// 
+/// If the number starts with "9", the second digit selects the motor
+/// to re-do the calibration. 
 void decodeCommand()
 {
-  // Add documentation on how this works
-
-  long command = Serial.parseInt();
+    long command = Serial.parseInt();
 
   // Empty serial port
   for (int n=0; n<Serial.available(); n++)
@@ -135,34 +145,39 @@ void decodeCommand()
   switch (motor)
   {
   case 1:
+    nema17.moveRun(movement);
+    motor_name = "nema17";
+
+  case 2:
+    nema23.moveRun(movement);
+    motor_name = "nema23";
+  
+  case 3:
     inclineActuator.moveToPercentage(movement);
     motor_name = "Incline actuator";
     break;
 
-  case 2:
-    nema17.moveRun(movement);
-    motor_name = "nema17";
-    break;
-
-  case 3:
-    nema23.moveRun(movement);
-    motor_name = "nema23";
-    break;
-   
    case 4:
     elevatorActuator.moveToPercentage(movement);
     motor_name = "Elevator Actuator";
     break;
 
-  case 4:
-    // Implement elevator actuator
-
   case 9:
-    // Implement find range with first two digits
-    // 9100 : Calibrate motor  case 1
+    int motor_calibration = abs(floor(movement/100));
+    
+    switch (motor_calibration)
+    {
+    case 1: nema17.findRange(); break;   
+    case 2: nema23.findRange(); break;
+    case 3: inclineActuator.findRange(); break;
+    case 4: elevatorActuator.findRange(); break;
+    default: Serial.println("Incorrect command to calibrate"); break;
+    }
+    
+    Serial.print("Recalibrating: " + String(motor_name));
 
   default:
-    Serial.println("Wrong command: " + String(command));
+    Serial.println("Incorrect command: " + String(command));
     break;
   }
 
